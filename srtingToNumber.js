0e06d5af0b48177d94d5f43ca7637e5391697958
Qthyami <Anipabedit@gmail.com>
2023-02-13 21:15:39 +0300

    first commit

---
+++ srtingToNumber.js
@@ -0,0 +1,13 @@
+function stringToNumber (str){
+  return parseInt(str);
+}
+ 
+
+
+  
+function stringToNumber (str){
+  // put your code here
+  return Number(str);
+}
+
+  console.log(stringToNumber("345345"));
