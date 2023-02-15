62c61333b11f2e6e795fcbdeb6b6e89f6a422f67
Qthyami <Anipabedit@gmail.com>
2023-02-15 08:24:03 +0300

    Creation

---
+++ hello_username.js
@@ -0,0 +1,10 @@
+
+function greet(name) {
+    //your code here
+    let answ1= 'Hello, ';
+    let answ2 = " how are you doing today?";
+    let answer = answ1 + name + answ2;
+    return answer;
+  }
+
+ console.log (greet("Ryan"));
