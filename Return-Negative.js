4d5fc4f5915aed80a13ffae79add95b283279418
Qthyami <Anipabedit@gmail.com>
2023-02-16 08:31:33 +0300

    first commit

---
+++ Return-Negative.js
@@ -0,0 +1,14 @@
+function makeNegative(num) {
+    // Code?
+    if (num>0) {
+      return -num;
+    } 
+      return num;
+    
+  }
+  console.log(makeNegative(-15));
+
+  function makeNegative(num) {
+    return num > 0 ? -num : num;
+  }
+  
---
+++ km-h-to-sm-s.js
@@ -0,0 +1,6 @@
+function cockroachSpeed(s) {
+    //Good Luck!
+    let v = s * (10/36)*100;
+    return Math.floor(v);
+  }
+  console.log (cockroachSpeed(36));
